5f2ad8c390585c36e28b152e139e8828697be116
D7703_16 <d7703_16@D7703-16ui-iMac.local>
2018-04-11 12:18:23 +0900

    Closure

--- Digital Timer/ViewController.swift
+++ Digital Timer/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Digital Timer
-//
-//  Created by 김종현 on 2018. 4. 4..
-//  Copyright © 2018년 김종현. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -16,13 +8,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateTime),userInfo: nil, repeats: true)
+        
+        //클로저(Closure)의 사용
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {
+            (myTimer) in
+            self.updateTime()
+            })
     }
     
     @objc func updateTime() {
         let date = Date()
         let formatter = DateFormatter()
-        formatter.dateFormat = "HH:mm:ss EEE"
+        formatter.dateFormat = "HH:mm:ss EEE YYYY"
         timeLabel.text = formatter.string(from: date)
     }
 
