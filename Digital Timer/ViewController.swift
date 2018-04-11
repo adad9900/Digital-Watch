33421563235562907c1e8aa17d955734190d9664
왕휘재 | Hwijae Wang <37130260+adad9900@users.noreply.github.com>
2018-04-11 12:38:31 +0900

    Update ViewController.swift

--- Digital Timer/ViewController.swift
+++ Digital Timer/ViewController.swift
@@ -1,28 +1,44 @@
+
+
 import UIKit
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timerSwitch: UISwitch!
     var myTimer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateTime),userInfo: nil, repeats: true)
         
-        //클로저(Closure)의 사용
-        myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {
-            (myTimer) in
-            self.updateTime()
-            })
+        // switch off
+        timerSwitch.setOn(false, animated: true)
+        
+//        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
     }
     
-    @objc func updateTime() {
+    func updateTime() {
         let date = Date()
         let formatter = DateFormatter()
-        formatter.dateFormat = "HH:mm:ss EEE YYYY"
+        formatter.dateFormat = "HH:mm:ss a"
         timeLabel.text = formatter.string(from: date)
+//        timeLabel.text = String(describing: date)
     }
 
+    @IBAction func timerStrat(_ sender: Any) {
+        if timerSwitch.isOn == true {
+//            myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+            
+            // closure 사용
+            myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in
+                self.updateTime()
+            })
+            
+        } else {
+            myTimer.invalidate()
+            
+        }
+    }
 }
 
